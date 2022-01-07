59e57dd18f8d89fe49cff7463dd27665f44fbe84
Asha Nair <ashanair93@icloud.com>
2022-01-07 13:18:06 -0500

    Created project

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by bubu on 2022-01-07.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
